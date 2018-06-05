3af3a72404ed346a793ba5b55648594bcc78e6a6
Yale96 <yannickvanleeuwen@i-lion.nl>
2018-06-05 15:18:35 +0200

    Basic configs

---
+++ servicetwo/src/test/java/com/servicetwo/ServicetwoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.servicetwo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringApplicationConfiguration(classes = Runner.class)
+//@WebAppConfiguration
+//public class ServiceTwoApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
