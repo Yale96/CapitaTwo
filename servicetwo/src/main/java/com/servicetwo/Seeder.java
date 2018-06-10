3137f27dfba7ea1de65b3701df223a94cefbb42e
Yale96 <yannickvanleeuwen@i-lion.nl>
2018-06-10 14:35:14 +0200

    seeder

--- servicetwo/src/main/java/com/servicetwo/Seeder.java
+++ servicetwo/src/main/java/com/servicetwo/Seeder.java
@@ -28,76 +28,78 @@ import org.springframework.stereotype.Component;
 @Component
 public class Seeder implements ApplicationRunner {
 
-    private UserRepository userRepository;
-
-    @Autowired
-    public Seeder(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public void run(ApplicationArguments aa) {
-    }
-
-//    private SubjectRepository subjectRepository;
-//    private SubjectRequest subjectRequest;
-//    private NewsRepository newsRepository;
-//    private UserRequest userRequest;
 //    private UserRepository userRepository;
-//    
+//
 //    @Autowired
-//    public Seeder(SubjectRepository subjectRepository, NewsRepository newsRepository, UserRepository userRepository)
-//    {
-//        subjectRequest = new SubjectRequest();
-//        userRequest = new UserRequest();
-//        this.subjectRepository = subjectRepository;
-//        this.newsRepository = newsRepository;
+//    public Seeder(UserRepository userRepository) {
 //        this.userRepository = userRepository;
 //    }
 //
 //    @Override
 //    public void run(ApplicationArguments aa) {
-//        
-//        
-//        
-//        for(Subject s: subjectRequest.getAllFromOne())
-//        {
-//            Subject toPersist = new Subject(s.getNaam(), s.getOmschrijving(), s.getAgeLimit());
-//            subjectRepository.save(toPersist);
-//            String ss = "Debug";
-//        }
-//        
-//        
-//        
-//        for(User u: userRequest.getAllFromOne())
-//        {
-//            User toPersist = new User(u.getName(), u.getMail(), u.getPassword(), u.getAge());
-//            for(Subject s: userRequest.getAllSubjectsPerUserFromOne(u.getName()))
-//            {
-//                List<Subject> toAdd = new ArrayList<>();
-//                Subject addHere = subjectRepository.findSingleSubjectByNaam(s.getNaam());
-//                toAdd.add(addHere);
-//                toPersist.setFollowingSubjects(toAdd);
-////                if(subjectRepository.findSingleSubjectByNaam(s.getNaam()) == null)
-////                {
-////                    subjectRepository.save(s);
-////                }
-////                else
-////                    subjectRepository.save(s);
-//            }
-//            userRepository.save(toPersist);
-//        }
-//        
-//        List<Subject> subjects = subjectRepository.findAll();
-//        
-//        for(Subject s: subjects)
-//        {
-//            News n = new News();
-//            n.setTitel(s.getNaam() + " in het nieuws");
-//            n.setContent(s.getNaam() + " heel mooi contentje dit.");
-//            n.setAgeLimit(0);
-//            n.setSubject(s);
-//            newsRepository.save(n);
-//        }
 //    }
+    
+    
+
+    private SubjectRepository subjectRepository;
+    private SubjectRequest subjectRequest;
+    private NewsRepository newsRepository;
+    private UserRequest userRequest;
+    private UserRepository userRepository;
+    
+    @Autowired
+    public Seeder(SubjectRepository subjectRepository, NewsRepository newsRepository, UserRepository userRepository)
+    {
+        subjectRequest = new SubjectRequest();
+        userRequest = new UserRequest();
+        this.subjectRepository = subjectRepository;
+        this.newsRepository = newsRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(ApplicationArguments aa) {
+        
+        
+        
+        for(Subject s: subjectRequest.getAllFromOne())
+        {
+            Subject toPersist = new Subject(s.getNaam(), s.getOmschrijving(), s.getAgeLimit());
+            subjectRepository.save(toPersist);
+            String ss = "Debug";
+        }
+        
+        
+        
+        for(User u: userRequest.getAllFromOne())
+        {
+            User toPersist = new User(u.getName(), u.getMail(), u.getPassword(), u.getAge());
+            for(Subject s: userRequest.getAllSubjectsPerUserFromOne(u.getName()))
+            {
+                List<Subject> toAdd = new ArrayList<>();
+                Subject addHere = subjectRepository.findSingleSubjectByNaam(s.getNaam());
+                toAdd.add(addHere);
+                toPersist.setFollowingSubjects(toAdd);
+//                if(subjectRepository.findSingleSubjectByNaam(s.getNaam()) == null)
+//                {
+//                    subjectRepository.save(s);
+//                }
+//                else
+//                    subjectRepository.save(s);
+            }
+            userRepository.save(toPersist);
+        }
+        
+        List<Subject> subjects = subjectRepository.findAll();
+        
+        for(Subject s: subjects)
+        {
+            News n = new News();
+            n.setTitel(s.getNaam() + " in het nieuws");
+            n.setContent(s.getNaam() + " heel mooi contentje dit.");
+            n.setAgeLimit(0);
+            n.setSubject(s);
+            newsRepository.save(n);
+        }
+    }
 }
